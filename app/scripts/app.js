eea006a504ed2b828cd69fb82958ffed2725ac25
Carlos A. Gomes <carlos.algms@gmail.com>
2015-06-27 18:33:50 -0300

    angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name g4cClocksApp
+ * @description
+ * # g4cClocksApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('g4cClocksApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name g4cClocksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the g4cClocksApp
+ */
+angular.module('g4cClocksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
